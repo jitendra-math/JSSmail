e174fdf4e27ca8a5ccd694f47d97a36965dcf193
Jitendra Singh Nimod <i.jitendra.singh0@gmail.com>
2026-02-27 12:27:41 +0530

    Add server-side redirect logic based on session

---
+++ src/routes/+page.server.js
@@ -0,0 +1,10 @@
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ locals }) => {
+	if (locals.session) {
+		throw redirect(302, '/inbox');
+	} else {
+		throw redirect(302, '/login');
+	}
+};
+
